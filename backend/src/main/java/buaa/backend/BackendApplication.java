427b1f67d2d25f9dc0efcc4b0196420cd5c03823
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-05-19 22:07:13 +0800

    init backend

---
+++ backend/src/main/java/buaa/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package buaa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
