8aa1ae47b758b498eb1fb885d2f0573789d1aad2
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-05-27 10:21:17 +0800

    add and test config for database

--- backend/src/main/java/buaa/backend/Test.java
+++ backend/src/main/java/buaa/backend/Test.java
@@ -1,13 +1,24 @@
 package buaa.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class Test {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public List hello() {
+//        String sql = "insert into test (name) values ('zhangsan')";
+//        jdbcTemplate.execute(sql);
+        List res=jdbcTemplate.queryForList("select * from test");
+//        System.out.println("执行完成");
+        return res;
     }
 }
