55d39abf68a9e499ff5068c2659c055f7549990a
Lepitar <1585576796@qq.com>
2020-06-14 02:33:50 +0800

    fix

--- frontend/src/views/Repair.vue
+++ frontend/src/views/Repair.vue
@@ -87,13 +87,14 @@ export default {
         "评价内容",
         "评分"
       ],
-      repairerInfo: [
+      repairerInfo: [],
+      /*repairerInfo: [
         {
-          username: "1234567",
+          username: "",
           name: "",
           tel: ""
         }
-      ],
+      ],*/
       selectedRepairerUsername: "",
       workOrderForm: {
         repairId: "",
