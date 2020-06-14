33acc122464d6f2b768b971f9c62dc83c78b350b
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-06-14 11:24:18 +0800

    rm el-ui

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import VDistpicker from 'v-distpicker';
 
