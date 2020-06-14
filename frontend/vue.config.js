7db1ee6daa9e765a4499015a73641169b7e98074
RyougiT <1724707236@qq.com>
2020-06-14 09:27:11 +0800

    cdn speedup

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+// for not load lib file test
+module.exports = {
+  publicPath: './',
+  productionSourceMap: false,
+  configureWebpack: config => {
+    config.externals = {
+      'element-ui': 'ELEMENT',
+      vue: 'Vue',
+      axios: 'axios',
+      'vue-router': 'VueRouter',
+    };
+  },
+};
