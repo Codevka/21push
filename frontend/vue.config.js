6572b1f5b6005252f05f05a3cade21e594909802
RyougiT <1724707236@qq.com>
2020-06-16 12:23:44 +0800

    add splitChunks

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,14 +2,40 @@
 module.exports = {
   // publicPath: './',
   productionSourceMap: false,
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     config.externals = {
+      'core-js': 'core-js',
       'element-ui': 'ELEMENT',
       vue: 'Vue',
       axios: 'axios',
       vuex: 'Vuex',
       'vue-router': 'VueRouter',
-      'crypto-js': 'CryptoJS'
+      'crypto-js': 'CryptoJS',
+      'websocket-extensions': 'Extensions',
+      'websocket-driver': 'Driver',
     };
+    let optimization = {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000, // 依赖包超过20000bit将被单独打包
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      },
+    };
+    Object.assign(config, {
+      optimization,
+    });
   },
 };
