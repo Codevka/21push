e59beab5d9a77063a7d88c25bc9231dbfe5410dd
RyougiT <1724707236@qq.com>
2020-06-16 10:09:10 +0800

    cdn test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,15 @@
-// // for not load lib file test
-// module.exports = {
-//   publicPath: './',
-//   productionSourceMap: false,
-//   configureWebpack: config => {
-//     config.externals = {
-//       'element-ui': 'ELEMENT',
-//       vue: 'Vue',
-//       axios: 'axios',
-//       vuex: 'Vuex',
-//       'vue-router': 'VueRouter',
-//       'v-distpicker': 'VDistpicker'
-//     };
-//   },
-// };
+// for not load lib file test
+module.exports = {
+  publicPath: './',
+  productionSourceMap: false,
+  configureWebpack: config => {
+    config.externals = {
+      'element-ui': 'ELEMENT',
+      vue: 'Vue',
+      axios: 'axios',
+      vuex: 'Vuex',
+      'vue-router': 'VueRouter',
+      'v-distpicker': 'VDistpicker'
+    };
+  },
+};
