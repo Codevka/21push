a51b3bc09ac6982a5cad2d95c31d0cd99de8d80b
RyougiT <1724707236@qq.com>
2020-06-16 13:31:26 +0800

    add webpack.base.conf.js

---
+++ frontend/webpack.base.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  externals: {
+    'core-js': 'core-js',
+    'element-ui': 'ELEMENT',
+    vue: 'Vue',
+    axios: 'axios',
+    vuex: 'Vuex',
+    'vue-router': 'VueRouter',
+    'crypto-js': 'CryptoJS',
+    'websocket-extensions': 'Extensions',
+    'websocket-driver': 'Driver',
+    'sockjs-client': 'SockJS',
+  },
+};
