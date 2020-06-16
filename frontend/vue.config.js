6c9aa90c06db3d564a1fd387fadfa8fdcbf61476
RyougiT <1724707236@qq.com>
2020-06-16 14:16:58 +0800

    disable splitChunk optimize

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -15,28 +15,28 @@ module.exports = {
       'websocket-driver': 'Driver',
       'sockjs-client': 'SockJS'
     };
-    let optimization = {
-      runtimeChunk: 'single',
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 20000, // 依赖包超过20000bit将被单独打包
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              // get the name. E.g. node_modules/packageName/not/this/part.js
-              // or node_modules/packageName
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `npm.${packageName.replace('@', '')}`;
-            },
-          },
-        },
-      },
-    };
-    Object.assign(config, {
-      optimization,
-    });
+    // let optimization = {
+    //   runtimeChunk: 'single',
+    //   splitChunks: {
+    //     chunks: 'all',
+    //     maxInitialRequests: Infinity,
+    //     minSize: 20000, // 依赖包超过20000bit将被单独打包
+    //     cacheGroups: {
+    //       vendor: {
+    //         test: /[\\/]node_modules[\\/]/,
+    //         name(module) {
+    //           // get the name. E.g. node_modules/packageName/not/this/part.js
+    //           // or node_modules/packageName
+    //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+    //           // npm package names are URL-safe, but some servers don't like @ symbols
+    //           return `npm.${packageName.replace('@', '')}`;
+    //         },
+    //       },
+    //     },
+    //   },
+    // };
+    // Object.assign(config, {
+    //   optimization,
+    // });
   },
 };
